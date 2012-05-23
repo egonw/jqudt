c8fd8c6dda359bb9dfc2924bf607bf8e401acb73
Egon Willighagen <egon.willighagen@gmail.com>
2012-05-23 07:32:10 +0200

    Changed the API to allow reading of multiple ontology modules into the same Repository

--- src/main/java/com/github/jqudt/onto/OntoReader.java
+++ src/main/java/com/github/jqudt/onto/OntoReader.java
@@ -10,22 +10,16 @@ import java.io.InputStream;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
-import org.openrdf.repository.sail.SailRepository;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFParseException;
-import org.openrdf.sail.memory.MemoryStore;
 
 public class OntoReader {
 
-	public static Repository read(String ontology) throws RepositoryException, RDFParseException, IOException {
-		Repository repos = new SailRepository(new MemoryStore());
-		repos.initialize();
+	protected static void read(Repository repos, String ontology) throws RepositoryException, RDFParseException, IOException {
 		RepositoryConnection con = repos.getConnection();
 
 		String filename = "onto/" + ontology;
         InputStream ins = OntoReader.class.getClassLoader().getResourceAsStream(filename);
 		con.add(ins, "", RDFFormat.RDFXML);
-
-		return repos;
 	}
 }
--- src/test/java/com/github/jqudt/onto/OntoReaderTest.java
+++ src/test/java/com/github/jqudt/onto/OntoReaderTest.java
@@ -11,13 +11,17 @@ import junit.framework.Assert;
 import org.junit.Test;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.sail.SailRepository;
 import org.openrdf.rio.RDFParseException;
+import org.openrdf.sail.memory.MemoryStore;
 
 public class OntoReaderTest {
 
 	@Test
 	public void testUnitOntology() throws RepositoryException, RDFParseException, IOException {
-		Repository repos = OntoReader.read("unit");
+		Repository repos = new SailRepository(new MemoryStore());
+		repos.initialize();
+		OntoReader.read(repos, "unit");
 		Assert.assertNotNull(repos);
 	}
 
