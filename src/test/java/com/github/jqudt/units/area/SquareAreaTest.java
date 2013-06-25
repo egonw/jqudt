6e2d8bdecc8b896659341b0300675eb2a2fed22d
Egon Willighagen <egon.willighagen@gmail.com>
2013-06-25 14:45:56 +0200

    Added a square Ångström unit

---
+++ src/test/java/com/github/jqudt/units/area/SquareAreaTest.java
@@ -0,0 +1,22 @@
+/* Copyright (C) 2013  Egon Willighagen <egonw@users.sf.net>
+ *
+ * License: new BSD
+ */
+package com.github.jqudt.units.area;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.github.jqudt.Quantity;
+import com.github.jqudt.onto.units.AreaUnit;
+
+public class SquareAreaTest {
+
+	@Test
+	public void testMinusTwenty() throws IllegalArgumentException, IllegalAccessException {
+		Quantity area = new Quantity(5, AreaUnit.SQUARE_ANGSTROM);
+		Quantity area2 = area.convertTo(AreaUnit.SQUARE_METER);
+		Assert.assertEquals(0.00000000000000000005, area2.getValue(), 0.01);
+	}
+
+}
