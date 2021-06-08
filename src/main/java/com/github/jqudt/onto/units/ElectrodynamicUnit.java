774414534dea716376e26c01ad3bf17dcc7ad40e
leebphil <leebphil@dev>
2021-06-08 14:58:14 +0200

    added ElectrodynamicUnit to give some standard units of the field of electro dynamic

---
+++ src/main/java/com/github/jqudt/onto/units/ElectrodynamicUnit.java
@@ -0,0 +1,54 @@
+package com.github.jqudt.onto.units;
+
+import com.github.jqudt.Unit;
+import com.github.jqudt.onto.UnitFactory;
+
+public class ElectrodynamicUnit {
+
+    private ElectrodynamicUnit() {}
+
+    // electric current
+    public static Unit PicoAmpere = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/PicoA");
+    public static Unit NanoAmpere = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/NanoA");
+    public static Unit MicroAmpere = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/MicroA");
+    public static Unit MilliAmpere = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/MilliA");
+    public static Unit Ampere = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/A");
+    public static Unit Abampere = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/A_Ab");
+    public static Unit KiloAmpere = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/KiloA");
+
+    // electric charge
+    public static Unit AttoCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/AttoC");
+    public static Unit FemtoCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/FemtoC");
+    public static Unit ZeptoCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/ZeptoC");
+    public static Unit YoctoCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/YoctoC");
+    public static Unit PicoCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/PicoC");
+    public static Unit NanoCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/NanoC");
+    public static Unit MicroCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/MicroC");
+    public static Unit MilliCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/MilliC");
+    public static Unit CentiCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/CentiC");
+    public static Unit DeciCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/DeciC");
+    public static Unit Coulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/C");
+    public static Unit DecaCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/DecaC");
+    public static Unit HectoCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/HectoC");
+    public static Unit KiloCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/KiloC");
+    public static Unit MegaCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/MegaC");
+    public static Unit GigaCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/GigaC");
+    public static Unit TeraCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/TeraC");
+    public static Unit PetaCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/PetaC");
+    public static Unit YottaCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/YottaC");
+    public static Unit ZettaCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/ZettaC");
+    public static Unit ExaCoulomb = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/ExaC");
+
+
+    // electric resistance
+    public static Unit MicroOhm = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/MicroOHM");
+    public static Unit MilliOhm = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/MilliOHM");
+    public static Unit Ohm = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/OHM");
+    public static Unit KiloOhm = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/KiloOHM");
+    public static Unit MegaOhm = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/MegaOHM");
+    public static Unit GigaOhm = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/GigaOHM");
+    public static Unit TeraOhm = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/TeraOHM");
+
+    // electric voltage
+    public static Unit Volt = UnitFactory.getInstance().getUnit("http://qudt.org/vocab/unit/V");
+}
