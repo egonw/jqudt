c6c6c56bc08298adc587664c041c9a96402047e4
leebphil <leebphil@dev>
2021-06-09 11:07:30 +0200

    added property of isDeltaQuantity added conversion of absolute units

--- src/main/java/com/github/jqudt/onto/QUDT.java
+++ src/main/java/com/github/jqudt/onto/QUDT.java
@@ -29,5 +29,6 @@ public class QUDT {
 	public final static URI DERIVED_UNIT = getURI("DerivedUnit");
 	public final static URI NOT_USED_WITH_SI_UNIT = getURI("NotUsedWithSIUnit");
 	public final static URI USED_WITH_SI_UNIT = getURI("UsedWithSIUnit");
+	public final static URI IS_DELTA_QUANTITY = getURI("isDeltaQuantity");
 
 }
